91ffa1a102ac6d71558e8983229d4a21885bca04
dnallor1 <100855447+dnallor1@users.noreply.github.com>
2022-05-09 21:37:03 +0200

    update

--- Trxtures/main.cpp
+++ Trxtures/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <cmath>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-
 class CustomRectangleShape : public sf::RectangleShape {
 public:
-    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f &position) : sf::RectangleShape(size)
-    {
+    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f &position) : sf::RectangleShape(size)    {
         setPosition(position);
     }
-    void setSpeed(const int& x_speed, const int& y_speed) {
+    void setSpeed(const int& x_speed, const int& y_speed, const int & ro_speed) {
         x_speed_ = x_speed;
         y_speed_ = y_speed;
+        ro_speed_ = ro_speed;
     }
     void animate(const sf::Time &elapsed){
-        bouncce();
+        bounce();
         float dt = elapsed.asSeconds();
-        move(x_speed_*dt,y_speed_*dt);
-        rotate(ro_speed_*dt);
+        move(x_speed_ * dt, y_speed_ * dt);
     }
     void setBounds(const float& l_bound, const float& r_bound,const float& u_bound,const float& d_bound){
         l_bound_  = l_bound  ;
@@ -26,31 +25,28 @@ public:
         u_bound_  = u_bound  ;
         d_bound_  = d_bound  ;
     }
-    void moveInDirection(float dt, const sf::Keyboard::Key &key) {
-        sf::FloatRect bounds = getGlobalBounds();
-        if (key == sf::Keyboard::Up and bounds.top > 0) {
-            bouncce();
-            move(0, -y_speed_ * dt);
-        } else if (key == sf::Keyboard::Down and
-                   bounds.top + bounds.height > window_size.y) {
-            bouncce();
+    void moveInDirection(const sf::Time &elapsed, const sf::Keyboard::Key &key)      {
+        float dt = elapsed.asSeconds();
+        if(key == sf::Keyboard::Up) {
+            y_speed_ = -1*abs(y_speed_);
+            bounce();
             move(0, y_speed_ * dt);
-        } else if (key == sf::Keyboard::Left and bounds.left > 0) {
-            bouncce();
-            move(-x_speed_ * dt, 0);
-        } else if (key == sf::Keyboard::Right and
-                   bounds.left + bounds.width > window_size.x) {
-            bouncce();
+        }
+        else if(key == sf::Keyboard::Down) {
+            y_speed_ = abs(y_speed_);
+            bounce();
+            move(0, y_speed_ * dt);
+        }
+        else if(key == sf::Keyboard::Left)  {
+            x_speed_ = -1*abs(x_speed_);
+            bounce();
             move(x_speed_ * dt, 0);
         }
-    }
-    bool isClicked(const sf::Vector2i &mouse_position) {
-        sf::FloatRect bounds = getGlobalBounds();
-        if(mouse_position.x >= bounds.left && mouse_position.x <= bounds.left + bounds.width
-                && mouse_position.y >= bounds.top && mouse_position.y <= bounds.top + bounds.height) {
-            return true;
+        else if(key == sf::Keyboard::Right) {
+            x_speed_ = abs(x_speed_);
+            bounce();
+            move(x_speed_ * dt, 0);
         }
-        return false;
     }
 private:
     int x_speed_ = 0 ;
@@ -60,144 +56,158 @@ private:
     float r_bound_ = 0;
     float u_bound_ = 0;
     float d_bound_ = 0;
-    sf::Vector2u window_size;
-    void bouncce(){
+    void bounce(){
         sf::FloatRect rectangle_bounds = getGlobalBounds();
         if(rectangle_bounds.top <= u_bound_){
             y_speed_ = abs(y_speed_);
         }
         if(rectangle_bounds.top + rectangle_bounds.height >= d_bound_){
             y_speed_ = abs(y_speed_) * -1;
-            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
         }
         if(rectangle_bounds.left <= l_bound_ ){
             x_speed_ = abs(x_speed_);
-            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
         }
         if(rectangle_bounds.left + rectangle_bounds.width >= r_bound_){
             x_speed_ = abs(x_speed_) * -1;
-            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
         }
     }
 };
-
 int main() {
-    // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
     std::vector<std::unique_ptr<sf::Drawable>> shapes;
-    sf::Vector2f size(60.0, 75.0);
-    sf::Vector2f position(120.0, 60.0);
-    // create some shapes
+    const float gridSize = 50.f;
+    sf::Vector2f vel_;
+    sf::Vector2i mos_Pos_;
+    sf::Vector2f size(40.0, 60.0);
+    sf::Vector2f position(520.0, 360.0);
     CustomRectangleShape rectangle(size, position);
-    rectangle.setPosition(500.0, 400.0);
-    rectangle.setFillColor(sf::Color(100, 50, 250));
-    rectangle.setSpeed(100, 150);
-
-    sf::Texture texture_guy;
-    if(!texture_guy.loadFromFile("guy.png")) { return 1; }
-    sf::Sprite guy;
-    guy.setTexture(texture_guy);
-    guy.setTextureRect(sf::IntRect(10, 20, 20, 15)); //left, top, width, height
-
+    rectangle.setSpeed(10, 10, 5);
+    std::srand(std::time(nullptr));
     sf::Texture texture_grass;
-    if (!texture_grass.loadFromFile("grass.png")) { return 1; }
-    texture_grass.setRepeated(true);
+    if (!texture_grass.loadFromFile("grass.png")) {
+        std::cerr << "Could not load texture" << std::endl;
+        return 1;
+    }
     sf::Sprite grass;
     grass.setTexture(texture_grass);
     grass.setTextureRect(sf::IntRect(0, 0, 800, 600));
+    texture_grass.setRepeated(true);
+
+    sf::Texture texture_guy;
+    if(!texture_guy.loadFromFile("guy.png")) {
+        return 1; }
+    sf::Sprite guy;
+    guy.setTexture(texture_guy);
+    guy.setTextureRect(sf::IntRect(20, 20, 20, 20)); //left, top, width, height
+    texture_guy.setRepeated(true);
 
     sf::Texture texture_wall;
-    if(!texture_wall.loadFromFile("wall.png")) { return 1; }
-    texture_wall.setRepeated(true);
+    if(!texture_wall.loadFromFile("wall.png")) {
+        return 1; }
+    rectangle.setTexture(&texture_guy);
+    rectangle.setBounds(0, window.getSize().x, 0, window.getSize().y);
+    std::vector<sf::Sprite> walls;
     sf::Sprite wall;
     wall.setTexture(texture_wall);
-    wall.setScale(0.9, 0.9);
-    wall.setPosition(50.0, 100.0);
-    wall.setTextureRect(sf::IntRect(0, 0, 50, 300));
+    wall.setTextureRect(sf::IntRect(0, 0, 40, 300));
+    texture_wall.setRepeated(true);
 
-    sf::Sprite wall2;
-    wall2.setTexture(texture_wall);
-    wall2.setScale(0.9, 0.9);
-    wall2.setPosition(175.0, 150.0);
-    wall2.setTextureRect(sf::IntRect(0, 0, 50, 400));
-
-    sf::Sprite wall3;
-    wall3.setTexture(texture_wall);
-    wall3.setScale(0.9, 0.9);
-    wall3.setPosition(550.0, 100.0);
-    wall3.setTextureRect(sf::IntRect(0, 0, 50, 400));
-
-    sf::Sprite wall4;
-    wall4.setTexture(texture_wall);
-    wall4.setScale(0.9, 0.9);
-    wall4.setPosition(275.0, 75.0);
-    wall4.setTextureRect(sf::IntRect(0, 0, 200, 50));
-
-    sf::Sprite wall5;
-    wall5.setTexture(texture_wall);
-    wall5.setScale(0.9, 0.9);
-    wall5.setPosition(550.0, 350.0);
-    wall5.setTextureRect(sf::IntRect(0, 0, 200, 50));
+    //wall_1
+    wall.setPosition(sf::Vector2f(50, 80));
+    walls.push_back(wall);
+    //wall_2;
+    wall.setPosition(sf::Vector2f(150, 220));
+    walls.push_back(wall);
+    //wall_3;
+    wall.setPosition(sf::Vector2f(250, 80));
+    walls.push_back(wall);
+    //wall_4;
+    wall.setPosition(sf::Vector2f(450, 300));
+    walls.push_back(wall);
+    //wall_5;
+    wall.setPosition(sf::Vector2f(700, 100));
+    walls.push_back(wall);
+    //wall_6;
+    wall.setPosition(sf::Vector2f(250, 235));
+    wall.setRotation(-90);
+    walls.push_back(wall);
+    //wall_7;
+    wall.setPosition(sf::Vector2f(70, 520));
+    wall.setRotation(-90);
+    walls.push_back(wall);
+    //wall_8;
+    wall.setPosition(sf::Vector2f(450, 520));
+    wall.setRotation(-90);
+    walls.push_back(wall);
+    //wall_9;
+    wall.setPosition(sf::Vector2f(440, 120));
+    wall.setRotation(-90);
+    walls.push_back(wall);
 
     sf::Clock clock;
-//    int rectangle_velocity_x = 5;
-//    int rectangle_velocity_y = 15;
-//    bool flag_y = false;
-//    bool flag_x = false;
-    rectangle.setTexture(&texture_guy);
-    // run the program as long as the window is open
+    sf::FloatRect new_Pos_;
     while (window.isOpen()) {
         sf::Time elapsed = clock.restart();
-        float dt = elapsed.asSeconds();
-        rectangle.setBounds(0, window.getSize().x, 0, window.getSize().y);
-//        sf::FloatRect rectangle_bounds = rectangle.getGlobalBounds();
-//        if(rectangle_bounds.top<=0 || rectangle_bounds.top+rectangle_bounds.height>=window.getSize().y)
-//        {
-//            if(flag_y != true)
-//            {
-//                rectangle_velocity_y *= -1;
-//                rectangle.setFillColor(sf::Color(rand() % 256,
-//                                                 rand() % 256,
-//                                                 rand() % 256));
-//            }
-//            flag_y = true;
-//        }
-//        else
-//            flag_y = false;
-//        if(rectangle_bounds.left<=0 || rectangle_bounds.left+rectangle_bounds.width>=window.getSize().x)
-//        {
-//            if(flag_x!=true)
-//            {
-//                rectangle_velocity_x *= -1;
-//                rectangle.setFillColor(sf::Color(rand() % 256,
-//                                                 rand() % 256,
-//                                                 rand() % 256));
-//            }
-//            flag_x = true;
-//        }
-//        else
-//            flag_x = false;
+        mos_Pos_.x = sf::Mouse::getPosition(window).x / (int)gridSize;
         sf::Event event;
         while (window.pollEvent(event)) {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if(event.type == sf::Event::KeyPressed) {
-            rectangle.moveInDirection(dt, event.key.code);
-            guy.move(dt, event.key.code);
+        //guy Movement
+        sf::Keyboard::Key pressed;
+        if(event.type == sf::Event::KeyPressed){
+            pressed = event.key.code;
+            //collision
+            for(auto &wall : walls) {
+                sf::FloatRect guyBounds = rectangle.getGlobalBounds();
+                sf::FloatRect wallBounds = wall.getGlobalBounds();
+                new_Pos_ = guyBounds;
+                new_Pos_.left += vel_.x;
+                new_Pos_.top += vel_.y;
+
+                if(wallBounds.intersects(new_Pos_)){
+                    vel_.y = 0.f;
+                    vel_.x = 0.f;
+                    //Bottom Collision
+                    if(guyBounds.top < wallBounds.top
+                            && guyBounds.top  + guyBounds.height < wallBounds.top  + wallBounds.height
+                            && guyBounds.left < wallBounds.left + wallBounds.width
+                            && guyBounds.left + guyBounds.width > wallBounds.left){
+                        rectangle.setPosition(guyBounds.left, wallBounds.top - guyBounds.height);
+                    }
+                    //Top Collision
+                    else if(guyBounds.top > wallBounds.top
+                            && guyBounds.top  + guyBounds.height > wallBounds.top  + wallBounds.height
+                            && guyBounds.left < wallBounds.left + wallBounds.width
+                            && guyBounds.left + guyBounds.width > wallBounds.left){
+                        rectangle.setPosition(guyBounds.left, wallBounds.top + wallBounds.height);
+                    }
+                    //Right Collision
+                    if(guyBounds.left < wallBounds.left
+                            && guyBounds.left + guyBounds.width < wallBounds.left + wallBounds.width
+                            && guyBounds.top < wallBounds.top + wallBounds.height
+                            && guyBounds.top + guyBounds.height > wallBounds.top){
+                        rectangle.setPosition(wallBounds.left - guyBounds.width, guyBounds.top);
+                    }
+                    //Left Collision
+                    else if(guyBounds.left > wallBounds.left
+                            && guyBounds.left + guyBounds.width > wallBounds.left + wallBounds.width
+                            && guyBounds.top < wallBounds.top + wallBounds.height
+                            && guyBounds.top + guyBounds.height > wallBounds.top){
+                        rectangle.setPosition(wallBounds.left + wallBounds.width, guyBounds.top);
+                    }
+                }
+                rectangle.animate(elapsed);
+                rectangle.moveInDirection(elapsed,pressed);
+            }
         }
-        // clear the window with black color
         window.clear(sf::Color::Black);
-        // draw everything here...
         window.draw(grass);
-        window.draw(wall);
-        window.draw(wall2);
-        window.draw(wall3);
-        window.draw(wall4);
-        window.draw(wall5);
+        for(auto &wall_ : walls) {
+            window.draw(wall_);
+        }
         window.draw(rectangle);
-        //        window.draw(guy);
         window.display();
     }
     return 0;
